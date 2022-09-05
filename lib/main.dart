688110583a85b1121a04f07457a73b206835c189
angon.alejandro@gmail.com <angon.alejandro@gmail.com>
2022-09-05 01:51:41 -0500

    Cambiacion una vez mas de titulo de tqm a chingas a tumadre

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter tqm angon 🤞',
+      title: 'Flutter y chingasa a tumadre',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter XDDDDD'),
+      home: const MyHomePage(title: 'Flutter y chingas a tu madre'),
     );
   }
 }
