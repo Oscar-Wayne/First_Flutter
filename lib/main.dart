9fcdbc294ffa90031168ad2dc5aa5fae9ae42ea6
angon.alejandro@gmail.com <angon.alejandro@gmail.com>
2022-09-05 01:41:07 -0500

    cambiacion de xd por xDDDDD

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter xd',
+      title: 'Flutter xdddd',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter XD'),
+      home: const MyHomePage(title: 'Flutter XDDDDD'),
     );
   }
 }
