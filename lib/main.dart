46459e1a75dfec13e4a5eb84fdba38ecb5797042
fero1420@gmail.com <fero1420@gmail.com>
2022-09-05 01:44:12 -0700

    Cambiacion de titulo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter xdddd',
+      title: 'Flutter tqm angon 🤞',
       theme: ThemeData(
         // This is the theme of your application.
         //
